e69a77dc54c43b9adf15ea355b1b70a822b4593c
yxmg <15900121620@163.com>
2019-03-11 10:31:00 +0800

    button组件的icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,22 +1,35 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{'icon-right': iconPosition === 'right'}">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
   export default {
-    name: "g-button"
+    name: "g-button",
+    props: ['icon', 'iconPosition']
   }
 </script>
 
 <style lang="scss" scoped>
   .g-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+
     height: var(--button-height);
     font-size: var(--font-size);
     background-color: var(--button-bg);
     border-radius: var(--border-radius);
     color: var(--color);
     border: 1px solid var(--border-color);
-    padding: 0 1em;
+    padding: 0 0.8em;
 
     &:hover {
       border-color: var(--border-color-hover);
@@ -28,6 +41,27 @@
 
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .3em;
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .3em;
+      }
+
+      > .content {
+        order: 1;
+      }
     }
   }
 

0ef60c401070698067db6c12eccd1cbc52b07c8c
yxmg <15900121620@163.com>
2019-03-11 18:25:46 +0800

    Button组件 - 使用chai添加4个测试用例 click的测试用例未完成

--- src/app.js
+++ src/app.js
@@ -16,3 +16,93 @@ new Vue({
     loading1: false,
   }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+// 单元测试
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount()
+
+  const useEl = vm.$el.querySelector('use')
+  const useHref = useEl.getAttribute('xlink:href')
+  expect(useHref).to.eq('#i-setting')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+// 测试loading
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      loading: true,
+    }
+  })
+  vm.$mount()
+
+  const useEl = vm.$el.querySelector('use')
+  const useHref = useEl.getAttribute('xlink:href')
+  expect(useHref).to.eq('#i-loading')
+}
+
+{
+  const divEl = document.createElement('div')
+  document.body.appendChild(divEl)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      iconPosition: 'right',
+    }
+  })
+  vm.$mount(divEl)
+
+  const svgEl = vm.$el.querySelector('svg')
+  const {order} = window.getComputedStyle(svgEl)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const divEl = document.createElement('div')
+  document.body.appendChild(divEl)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+    }
+  })
+  vm.$mount(divEl)
+
+  const svgEl = vm.$el.querySelector('svg')
+  const {order} = window.getComputedStyle(svgEl)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+// 能监听click - mock
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  vm.$mount()
+  vm.$on('click',() => {
+    console.log(1,"1")
+  })
+
+  const buttonEl = vm.$el
+  buttonEl.click()
+}

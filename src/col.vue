96cebb01afb54654f4c2ae907578c4b5cc1b382c
yxmg <15900121620@163.com>
2019-03-31 07:48:04 +0800

    feat:row和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="g-col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "g-col",
+    props: {
+      span: [Number, String],
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .g-col {
+    height: 100px;
+    background: lightsteelblue;
+    width: 50%;
+    border: 1px solid #000;
+
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "g-rol"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+  }
+</style>

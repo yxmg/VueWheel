9dd32cbe804a0c7636433158b88de330b94d2a5f
yxmg <15900121620@163.com>
2019-03-30 09:54:50 +0800

    feat:增加input组件及其样式

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import Vue from 'vue'
 import ButtonGroup from './button-group'
 import Button from './button'
 import Icon from './icon'
+import Input from './input'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 
 new Vue({
   el: '#app',
@@ -19,93 +21,5 @@ new Vue({
 
 import chai from 'chai'
 import spies from 'chai-spies'
+
 chai.use(spies)
-
-const expect = chai.expect
-// 单元测试1:含有Icon
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting'
-    }
-  })
-  vm.$mount()
-
-  const useEl = vm.$el.querySelector('use')
-  const useHref = useEl.getAttribute('xlink:href')
-  expect(useHref).to.eq('#i-setting')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-// 单元测试2：是否能loading
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      loading: true,
-    }
-  })
-  vm.$mount()
-
-  const useEl = vm.$el.querySelector('use')
-  const useHref = useEl.getAttribute('xlink:href')
-  expect(useHref).to.eq('#i-loading')
-}
-// 单元测试3：能正确设置图标位置 右
-{
-  const divEl = document.createElement('div')
-  document.body.appendChild(divEl)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      iconPosition: 'right',
-    }
-  })
-  vm.$mount(divEl)
-
-  const svgEl = vm.$el.querySelector('svg')
-  const {order} = window.getComputedStyle(svgEl)
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-// 单元测试4：能正确设置图标位置 左
-{
-  const divEl = document.createElement('div')
-  document.body.appendChild(divEl)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-    }
-  })
-  vm.$mount(divEl)
-
-  const svgEl = vm.$el.querySelector('svg')
-  const {order} = window.getComputedStyle(svgEl)
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-// 单元测试5：能监听click - mock
-{
-  const spy = chai.spy(() => {})
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting'
-    }
-  })
-  vm.$mount()
-  vm.$on('click',spy)
-
-  const buttonEl = vm.$el
-  buttonEl.click()
-  expect(spy).to.have.been.called
-}
---
+++ src/input.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input :value="value" type="text" class="g-input" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <g-icon v-if="error" name="error" class="icon-error"></g-icon>
+      <span class="text-error">{{error}}</span>
+    </template>
+
+  </div>
+
+</template>
+
+<script>
+  import Icon from './icon'
+
+  export default {
+    name: "g-input",
+    props: {
+      value: String,
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      readonly: {
+        type: Boolean,
+        default: false,
+      },
+      error: String,
+    },
+    components: {
+      'g-icon': Icon,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $height: 32px;
+  $border-color: #999;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $font-size: 12px;
+  $box-shadow-color: rgba(0, 0, 0, 0.5);
+  $color-error: #F1453D;
+  .wrapper {
+    display: inline-flex;
+    align-items: center;
+    font-size: $font-size;
+
+    > :not(:last-child) {
+      margin-right: 0.5em;
+    }
+
+    .g-input {
+      height: $height;
+      border: 1px solid $border-color;
+      border-radius: $border-radius;
+      padding: 0 8px;
+      font-size: inherit;
+
+      &:hover {
+        border: 1px solid $border-color-hover;
+      }
+
+      &:focus {
+        box-shadow: inset 0 1px 3px $box-shadow-color;
+        outline: none;
+      }
+
+      &[disabled], &[readonly] {
+        border-color: #aaa;
+        color: #aaa;
+        cursor: not-allowed;
+      }
+    }
+
+    &.error {
+      .g-input {
+        border-color: $color-error;
+      }
+
+      .icon-error {
+        fill: $color-error;
+      }
+
+      .text-error {
+        color: $color-error;
+      }
+    }
+  }
+</style>

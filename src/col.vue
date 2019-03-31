7e5ffb2e143300051e2ce242f5bbe22e68dc61b1
yxmg <15900121620@163.com>
2019-03-31 12:56:02 +0800

    feat(col):增加phone、ipad、narrowPc、widePc响应式处理

--- src/col.vue
+++ src/col.vue
@@ -27,8 +27,30 @@
   export default {
     name: "g-col",
     props: {
-      span: [Number, String],
-      offset: [Number, String],
+      span: {
+        type: [Number, String],
+        validator: checkIsLessThanTwentyFour
+      },
+      offset: {
+        type: [Number, String],
+        validator: checkIsLessThanTwentyFour
+      },
+      phone: {
+        type: Object,
+        validator: checkOnlySpanOrOffset
+      },
+      ipad: {
+        type: Object,
+        validator: checkOnlySpanOrOffset
+      },
+      narrowPc: {
+        type: Object,
+        validator: checkOnlySpanOrOffset
+      },
+      widePc: {
+        type: Object,
+        validator: checkOnlySpanOrOffset
+      }
     },
     data() {
       return {
@@ -37,8 +59,15 @@
     },
     computed: {
       colClass() {
-        const { span, offset } = this
-        return [span && `col-${span}`, offset && `offset-${offset}`]
+        const { span, offset, phone, ipad, narrowPc, widePc } = this
+
+        return [
+          ...this.createClasses({ span, offset }),
+          ...this.createClasses(phone, 'phone-'),
+          ...this.createClasses(ipad, 'ipad-'),
+          ...this.createClasses(narrowPc, 'narrow-pc-'),
+          ...this.createClasses(widePc, 'wide-pc-'),
+        ]
       },
       colStyle() {
         const { gutter } = this
@@ -47,7 +76,17 @@
           paddingRight: `${gutter / 2}px`
         }
       }
-    }
+    },
+
+    methods: {
+      createClasses(obj, str = '') {
+        if (!obj) return []
+        let result = []
+        obj.span && result.push(`col-${str}${obj.span}`)
+        obj.offset && result.push(`offset-${str}${obj.offset}`)
+        return result
+      }
+    },
   }
 </script>
 
@@ -67,6 +106,80 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
+      }
+    }
+
+    // 默认为PC尺寸，993px-1200px
+
+    // phone
+    @media (min-width: 576px) {
+      $class-prefix: col-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: offset-phone-;
+
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
+    // ipad
+    @media (min-width: 577px) and (max-width: 768px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: offset-ipad-;
+
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
+    // narrow-pc
+    @media (min-width: 769px) and (max-width: 992px) {
+      $class-prefix: col-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: offset-narrow-pc-;
+
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
+    // wide-pc
+    @media (min-width: 1201px) {
+      $class-prefix: col-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: offset-wide-pc-;
+
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
